8d8a297c407fbbfd733de094f756da54bed2493c
Aleksej <a.sinkevic2003@gmail.com>
2023-02-25 11:47:34 +0200

    update

--- app/src/main/java/com/example/testapplication/ImageList.java
+++ app/src/main/java/com/example/testapplication/ImageList.java
@@ -1,4 +1,61 @@
 package com.example.testapplication;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ImageList {
+    private static List<String> adIds;//stores all given ids
+    private final Context context;
+    public ImageList(Context context){
+        this.context = context;
+    }
+
+    public void getAdsIds(){
+        adIds = new ArrayList<>();
+        adIds.add("1");//test
+        adIds.add("2");//test
+        adIds.add("3");//test
+    }//will get ids from database
+
+    public List<Integer> getImages(){//will get images from database by given specific id
+
+        if (adIds.isEmpty()){
+            CharSequence text = "Too fast, you checked all ads";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            getAdsIds();
+        }
+        List<Integer> images = new ArrayList<>();
+
+        String adId = getRandomElement();
+
+        switch (adId) {
+            case "1":
+                images.add(R.drawable.five);
+                break;
+            case "2":
+                images.add(R.drawable.);
+                images.add(R.drawable);
+                break;
+            case "3":
+                images.add(R.drawable.images3);
+                break;
+        }
+        adIds.remove(adId);
+        return images;
+
+    }
+    public String getRandomElement()
+    {
+        Random r = new Random();
+
+        int randomItem = r.nextInt(adIds.size());
+        return  adIds.get(randomItem);
+    }
 }
--- app/src/main/java/com/example/testapplication/MainWindow.java
+++ app/src/main/java/com/example/testapplication/MainWindow.java
@@ -3,9 +3,7 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-import android.media.Image;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,36 +19,27 @@ public class MainWindow extends AppCompatActivity {
 
         ViewPager viewPager = findViewById(R.id.images);
 
-        List<Integer> list = new ArrayList<>();
-        list.add(R.drawable.images2);
-        list.add(R.drawable.images3);
-        ImageAdapter adapter = new ImageAdapter(this, list);
+        ImageList imageList = new ImageList(this);
+        imageList.getAdsIds();
+
+        ImageAdapter adapter = new ImageAdapter(this, imageList.getImages());
         viewPager.setAdapter(adapter);//refreshing photos
 
         ImageView backButton = findViewById(R.id.backButton);
         ImageView addButton = findViewById(R.id.addButton);
         ImageView menuButton = findViewById(R.id.menuButton);
-        TextView text = findViewById(R.id.text);
+        TextView text = findViewById(R.id.adText);
 
 
 
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                List<Integer> list = new ArrayList<>();
-                list.add(R.drawable.images);
-                list.add(R.drawable.images1);
-                adapter.setImageList(list);
-                adapter.notifyDataSetChanged();
-                viewPager.setAdapter(adapter);//refreshing photos
+        backButton.setOnClickListener(view -> finish());
 
-            }
+        addButton.setOnClickListener(view -> {
+
+            adapter.setImageList(imageList.getImages());
+            adapter.notifyDataSetChanged();
+            viewPager.setAdapter(adapter);//refreshing photos
+
         });
     }
 }
